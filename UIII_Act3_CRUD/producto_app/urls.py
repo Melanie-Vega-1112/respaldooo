708907ba92021dc03b71ce3199102d0ae3beccf8
Melanie-Vega-1112 <164534346+Melanie-Vega-1112@users.noreply.github.com>
2024-11-22 13:20:24 -0600

    Add files via upload

---
+++ UIII_Act3_CRUD/producto_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path 
+from producto_app import views
+
+urlpatterns = [
+    path("", views.inicio_vista,name="inicio_vista"),
+    path("registrarProducto/" ,views.registrarProducto,name="registrarProducto"),
+    path('eliminarProducto/<int:idproducto>/', views.eliminarProducto, name='eliminarProducto'),
+]
---
+++ UIII_Act3_CRUD/producto_app/views.py
@@ -0,0 +1,27 @@
+from .models import Producto
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+def inicio_vista(request):
+    losproductos = Producto.objects.all()
+    return render(request, "registrarProductos.html", {"misproductos": losproductos})
+
+def registrarProducto(request):
+    nombre=request.POST["txtnombre"]
+    idproducto=request.POST["txtid"]
+    Descripcion=request.POST["txtdesc"]
+    Precio=request.POST["txtprecio"]
+    Categoria=request.POST["txtcat"]
+    Material=request.POST["txtmat"]
+    stock=request.POST["txtstock"]
+
+    guardarproducto=Producto.objects.create(nombre=nombre, idproducto=idproducto, Descripcion=Descripcion, Precio=Precio, Categoria=Categoria, Material=Material, stock=stock)
+
+    return redirect("/")
+def eliminarProducto(request, idproducto):
+    if request.method == 'POST':
+        # Buscar el producto por ID y eliminarlo
+        producto = Producto.objects.get(idproducto=idproducto)
+        producto.delete()
+    # Redirigir de vuelta a la lista de productos después de eliminar
+    return redirect('registrarProductos') 
